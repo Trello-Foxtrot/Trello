d76a4b151f0427ad43e66df3a16aa86b143d9764
Ocheretian.Kateryna <226448@edu.p.lodz.pl>
2021-11-21 22:40:42 +0100

    basic colors

---
+++ lib/utils/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+const Color lightBlue = Color(0xffcae6ff);
+const Color darkBlue = Color(0xff004269);
+
+const Color lightGrey = Color(0xffb3b3b3);
+const Color darkGrey = Color(0xff535353);
+
+const Color darktextBlue = Color(0xff262f39);
