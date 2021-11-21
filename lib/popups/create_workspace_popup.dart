7ef860e7ea6961acb9e9a7bcc2eed4231f67092b
Ocheretian.Kateryna <226448@edu.p.lodz.pl>
2021-11-21 22:35:51 +0100

    create workspace popup

---
+++ lib/popups/create_workspace_popup.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:trello/buttons/blue_button.dart';
+import 'package:trello/buttons/cancel_button.dart';
+import 'package:trello/screens/main_screen.dart';
+
+class CreateWorkSpaceDialog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(50),
+      ),
+      elevation: 0.0,
+      backgroundColor: Colors.transparent,
+      child: Container(
+        width: 400,
+        height: 350,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.circular(50),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black26,
+              blurRadius: 10.0,
+              offset: Offset(0.0, 10.0),
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              const Padding(
+                padding: EdgeInsets.only(top: 40),
+                child: Text(
+                  "Let's build a Workspace",
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
+              TextFormField(
+                keyboardType: TextInputType.name,
+                autofocus: false,
+                decoration: const InputDecoration(
+                  hintText: 'Title',
+                  contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CancelButton(
+                    onClick: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  DialogBlueButton(
+                    text: "Create",
+                    onClick: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => MainScreen()),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
