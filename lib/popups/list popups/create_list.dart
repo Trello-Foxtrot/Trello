4fa4f5ddfe0e021c97bbffc2298acf608f6c9040
Ocheretian.Kateryna <226448@edu.p.lodz.pl>
2021-12-11 06:38:30 +0100

    create list popup

---
+++ lib/popups/list popups/create_list.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:trello/buttons/blue_button.dart';
+import 'package:trello/buttons/cancel_button.dart';
+import 'package:trello/screens/board_screen.dart';
+import 'package:trello/screens/main_screen.dart';
+import 'package:http/http.dart' as http;
+
+class CreateListDialog extends StatelessWidget {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final _titleController = TextEditingController();
+
+  // void addList(String name) {
+  //
+  //   Map<String, dynamic> map = new Map<String, dynamic>();
+  //   map['name'] = name;
+  //
+  //   http.post(
+  //     Uri.parse('http://localhost:8000/trello/List/add'),
+  //     body: map,
+  //   );
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(50),
+      ),
+      elevation: 0.0,
+      backgroundColor: Colors.transparent,
+      child: Container(
+        width: 400,
+        height: 350,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.rectangle,
+          borderRadius: BorderRadius.circular(50),
+          boxShadow: const [
+            BoxShadow(
+              color: Colors.black26,
+              blurRadius: 10.0,
+              offset: Offset(0.0, 10.0),
+            ),
+          ],
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(30.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                const Padding(
+                  padding: EdgeInsets.only(top: 40),
+                  child: Text(
+                    "Let's build a List",
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+                TextFormField(
+                  controller: _titleController,
+                  keyboardType: TextInputType.name,
+                  autofocus: false,
+                  decoration: const InputDecoration(
+                    hintText: 'Title',
+                    contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                  ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Please enter a title';
+                    }
+                    return null;
+                  },
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CancelButton(
+                      onClick: () {
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                    DialogBlueButton(
+                      text: "Create",
+                      onClick: () {
+                        if (_formKey.currentState!.validate()) {
+                          // addList(_titleController.text);
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => BoardScreen()),
+                          );
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
